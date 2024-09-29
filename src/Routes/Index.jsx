82acc8e1ce7b1525a4e116d8a12498d242544368
Sweta Raj patel <swetarajpatel2803@gmail.com>
2024-09-29 12:16:14 +0530

    setUp done

---
+++ src/Routes/Index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import App from '../App'
+import Home from '../Components/Home'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element :<App/>,
+        children :[
+            {
+                path : '',
+                element : <Home/>
+            }
+        ]
+    }
+])
+
+const Index = () => {
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default Index
